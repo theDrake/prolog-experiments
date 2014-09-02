f22c809c26fe6edcc7d50411b41de1236f2170c1
David C. Drake <dcdrake42@gmail.com>
2014-09-02 11:38:44 -0600

    Add "socrates.pl" and "riddle.pl"

---
+++ riddle.pl
@@ -0,0 +1,21 @@
+change(e,w).
+change(w,e).
+
+move([X,X,Goat,Cabbage],wolf,[Y,Y,Goat,Cabbage]) :- change(X,Y).
+move([X,Wolf,X,Cabbage],goat,[Y,Wolf,Y,Cabbage]) :- change(X,Y).
+move([X,Wolf,Goat,X],cabbage,[Y,Wolf,Goat,Y]) :- change(X,Y).
+move([X,Wolf,Goat,Cabbage],nothing,[Y,Wolf,Goat,Cabbage]) :- change(X,Y).
+
+oneEq(X,X,_).
+oneEq(X,_,X).
+
+safe([Man,Wolf,Goat,Cabbage]) :-
+  oneEq(Man,Goat,Wolf),
+  oneEq(Man,Goat,Cabbage).
+
+solution([e,e,e,e],[]).
+solution(Config,[Move|Rest]) :-
+  move(Config,Move,NextConfig),
+  safe(NextConfig),
+  solution(NextConfig,Rest).
+
