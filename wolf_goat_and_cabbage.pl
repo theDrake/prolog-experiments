d5cbe84d0a51a63e0b58ac3b42833ebc380a10f1
David C. Drake <dcdrake42@gmail.com>
2014-09-14 22:22:27 -0600

    Update "wolf_goat_and_cabbage.pl"
    Added comments to and improved the formatting of
    "wolf_goat_and_cabbage.pl".

--- wolf_goat_and_cabbage.pl
+++ wolf_goat_and_cabbage.pl
@@ -1,10 +1,14 @@
+% A Prolog program for solving the classic wolf, goat, and cabbage problem,
+% often known by other names, such as the fox, goose, and bag of beans puzzle
+% (http://en.wikipedia.org/wiki/Fox,_goose_and_bag_of_beans_puzzle).
+
 change(e,w).
 change(w,e).
 
-move([X,X,Goat,Cabbage],wolf,[Y,Y,Goat,Cabbage])          :- change(X,Y).
-move([X,Wolf,X,Cabbage],goat,[Y,Wolf,Y,Cabbage])          :- change(X,Y).
-move([X,Wolf,Goat,X],cabbage,[Y,Wolf,Goat,Y])             :- change(X,Y).
-move([X,Wolf,Goat,Cabbage],nothing,[Y,Wolf,Goat,Cabbage]) :- change(X,Y).
+move([X,X,Goat,Cabbage],    wolf,    [Y,Y,Goat,Cabbage])    :- change(X,Y).
+move([X,Wolf,X,Cabbage],    goat,    [Y,Wolf,Y,Cabbage])    :- change(X,Y).
+move([X,Wolf,Goat,X],       cabbage, [Y,Wolf,Goat,Y])       :- change(X,Y).
+move([X,Wolf,Goat,Cabbage], nothing, [Y,Wolf,Goat,Cabbage]) :- change(X,Y).
 
 oneEq(X,X,_).
 oneEq(X,_,X).
